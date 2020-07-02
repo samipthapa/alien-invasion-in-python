06fd0adb2d518a069505e353401d35189bee8de5
Samip Thapa <samipthapa99@gmail.com>
2020-07-02 17:42:51 +0545

    Ship that fires bullets

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings:
+    def __init__(self):
+        self.screen_width = 975
+        self.screen_height = 650
+        self.bg_color = (0, 0, 0)
+        self.ship_speed = 1.5
+
+        #Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 5
+        self.bullet_height = 20
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 4
+        
